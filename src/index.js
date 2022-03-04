4f8b3a0109318fb1204c2b431840d13bf1cdf9a0
+ <+>
2022-03-04 05:41:40 -0700

    Fetch for original toys

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  
+  //opens form to create new toy
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //fetch all of the current toys and add to the page
+
+  //fetch request
+  fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(data => console.log(data)) //function for adding cards will go here)
+  .catch(console.log("Error with data"))
+
+  //function for adding HTML elements
+
+
+
+
+
+
 });
