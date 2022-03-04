79222136640822f0a33d913979068289e9916c7b
+ <+>
2022-03-04 06:02:47 -0700

    Toy Card Template w. Filler Text

--- src/index.js
+++ src/index.js
@@ -20,11 +20,34 @@ document.addEventListener("DOMContentLoaded", () => {
   //fetch request
   fetch("http://localhost:3000/toys")
   .then(response => response.json())
-  .then(data => console.log(data)) //function for adding cards will go here)
+  .then(createToyCards()) //function for adding cards will go here)
   .catch(console.log("Error with data"))
 
   //function for adding HTML elements
+  function createToyCards(){
+    
+    const div = document.createElement('div')
+      div.classList.add('card')
+    const h2 = document.createElement('h2')
+      h2.textContent = 'h2 content text'
+    const img = document.createElement('img')
+      img.src = 'www.google.com'
+      img.classList.add('toy-avatar')
+    const p = document.createElement('p')
+      p.textContent = 'paragraph text'
+    const button = document.createElement("button")
+      button.textContent = 'button text'
+      button.classList.add('like-btn')
+      button.id = 'toyID'
 
+    toyCollection = document.querySelector('#toy-collection')
+    toyCollection.appendChild(div)
+    div.appendChild(h2)
+    div.appendChild(img)
+    div.appendChild(p)
+    div.appendChild(button)
+
+  }
 
 
 
