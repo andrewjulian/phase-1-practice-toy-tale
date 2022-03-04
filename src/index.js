f52d09a1a8a56efd7d87dd333db178b97cca0f89
+ <+>
2022-03-04 09:30:01 -0700

    Original Cards with Toy Data Populated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-let addToy = false;
-
 document.addEventListener("DOMContentLoaded", () => {
   
+  let addToy = false;
+
   //opens form to create new toy
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -18,39 +18,38 @@ document.addEventListener("DOMContentLoaded", () => {
   //fetch all of the current toys and add to the page
 
   //fetch request
-  fetch("http://localhost:3000/toys")
+  fetch("http://localhost:3000/toys/")
   .then(response => response.json())
-  .then(createToyCards()) //function for adding cards will go here)
-  .catch(console.log("Error with data"))
+  .then(data => createToyCards(data))
+  //.catch(console.log("Error with data"))
 
   //function for adding HTML elements
-  function createToyCards(){
-    
-    const div = document.createElement('div')
-      div.classList.add('card')
-    const h2 = document.createElement('h2')
-      h2.textContent = 'h2 content text'
-    const img = document.createElement('img')
-      img.src = 'www.google.com'
-      img.classList.add('toy-avatar')
-    const p = document.createElement('p')
-      p.textContent = 'paragraph text'
-    const button = document.createElement("button")
-      button.textContent = 'button text'
-      button.classList.add('like-btn')
-      button.id = 'toyID'
+  function createToyCards(object){
+    console.log(object)
+    console.log("Create Toys is Running")
+    for(const objKey in object){
+      console.log("This is the objKey" , objKey)
+      const div = document.createElement('div')
+        div.classList.add('card')
+      const h2 = document.createElement('h2')
+        h2.textContent = object[objKey].name
+      const img = document.createElement('img')
+        img.src = object[objKey].image
+        img.classList.add('toy-avatar')
+      const p = document.createElement('p')
+        p.textContent = object[objKey].likes
+      const button = document.createElement("button")
+        button.textContent = 'button text'
+        button.classList.add('like-btn')
+        button.id = object[objKey].id
 
-    toyCollection = document.querySelector('#toy-collection')
-    toyCollection.appendChild(div)
-    div.appendChild(h2)
-    div.appendChild(img)
-    div.appendChild(p)
-    div.appendChild(button)
-
+      toyCollection = document.querySelector('#toy-collection')
+      toyCollection.appendChild(div)
+      div.appendChild(h2)
+      div.appendChild(img)
+      div.appendChild(p)
+      div.appendChild(button)
+    }
   }
-
-
-
-
 
 });
